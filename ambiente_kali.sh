88420bfc3ac1f9d344351248cef8cb74120dff0d
Frankley Francalino da Rocha <contato.frankley@gmail.com>
2023-07-09 02:19:52 -0300

    ok

--- ambiente_kali.sh
+++ ambiente_kali.sh
@@ -21,13 +21,21 @@ sudo apt install -y \
 	python-tk \
 	python3-tk tk-dev
 
-curl https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 curl -s "https://get.sdkman.io" | bash
 
-source ~/.bashrc
+curl https://pyenv.run | bash
+
+if [ $SHELL -eq '/bin/zsh' ]
+then
+        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+        echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+        echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+        source ~/.zshrc
+else        
+        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+        echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+        echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+        source ~/.zshrc
+fi

f0bd8ea7969e3b8992d88760aea836cc76c9594c
Frankley Francalino da Rocha <rocha112358@gmail.com>
2023-09-18 18:28:48 -0300

    organizando

--- ambiente/kali-linux/all.sh
+++ ambiente/kali-linux/all.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#curl https://raw.githubusercontent.com/FrankleyRocha/scripts/main/ambiente/kali-linux/passo-1.sh | bash -il
-#curl https://raw.githubusercontent.com/FrankleyRocha/scripts/main/ambiente/kali-linux/passo-2.sh | bash -il
-#curl https://raw.githubusercontent.com/FrankleyRocha/scripts/main/ambiente/kali-linux/passo-3.sh | bash -il
-
 sudo apt update
 sudo apt install -y \
     zip \
